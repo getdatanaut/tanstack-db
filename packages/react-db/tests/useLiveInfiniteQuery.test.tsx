ccab040602ad2d7bc6fcc0a1e4ea969307728168
Marc MacLeod <marbemac+gh@gmail.com>
2026-06-16 13:47:16 +0200

    chore: apply lint import-order fixes from merge

--- packages/react-db/tests/useLiveInfiniteQuery.test.tsx
+++ packages/react-db/tests/useLiveInfiniteQuery.test.tsx
@@ -1,7 +1,11 @@
 import { describe, expect, it } from 'vitest'
 import { act, renderHook, waitFor } from '@testing-library/react'
-import { createCollection, createLiveQueryCollection, eq } from '@tanstack/db'
-import { BTreeIndex } from '@tanstack/db'
+import {
+  BTreeIndex,
+  createCollection,
+  createLiveQueryCollection,
+  eq,
+} from '@tanstack/db'
 import { useLiveInfiniteQuery } from '../src/useLiveInfiniteQuery'
 import { mockSyncCollectionOptions } from '../../db/tests/utils'
 import { createFilterFunctionFromExpression } from '../../db/src/collection/change-events'
